23466b42ff7361469ff6b756c9c6786878648d71
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-10-21 11:14:17 -0600

    Crea la clase padre titulo -crea la clase series que hereda de titulo -actualiza la clase peliculas que hereda de titulo

--- src/aluracursos/screenmatch/modelos/Peliculas.java
+++ src/aluracursos/screenmatch/modelos/Peliculas.java
@@ -1,71 +1,17 @@
 package aluracursos.screenmatch.modelos;
 
-public class Peliculas {
+public class Peliculas extends Titulo{
 
     //ATRIBUTOS
+    private String director;
 
-    private String nombreDePelicula;
-    private int fechaDeLanzamiento;
-    private int duracionMinutos;
-    private boolean incluidoEnPlan;
-    private double sumaDeLasEvaluaciones;
-    private int totalDeLasEvalaciones;
-
-    //METODOS SET
-
-    public void setNombreDePelicula(String nombreDePelicula) {
-        this.nombreDePelicula = nombreDePelicula;
+    //GETTERS
+    public String getDirector() {
+        return director;
     }
 
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public void setDuracionMinutos(int duracionMinutos) {
-        this.duracionMinutos = duracionMinutos;
-    }
-
-    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
-        this.incluidoEnPlan = incluidoEnPlan;
-    }
-
-    //METODOS GET
-
-    public int getTotalDeLasEvalaciones(){
-        return totalDeLasEvalaciones;
-    }
-
-    public String getNombreDePelicula() {
-        return nombreDePelicula;
-    }
-
-    public int getFechaDeLanzamiento() {
-        return fechaDeLanzamiento;
-    }
-
-    public int getDuracionMinutos() {
-        return duracionMinutos;
-    }
-
-    public boolean isIncluidoEnPlan() {
-        return incluidoEnPlan;
-    }
-
-    //METODOS
-
-    public void muestraFichaTecnica(){
-        System.out.println("El nombre de la pelicula es: " + nombreDePelicula);
-        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
-        System.out.println("Duracion en minutos: " + duracionMinutos);
-        System.out.println("Incluido en el plan: " + (incluidoEnPlan ? "Si" : "No"));
-    }
-
-    public void evalua(double nota){
-        sumaDeLasEvaluaciones += nota;
-        totalDeLasEvalaciones++;
-    }
-
-    public double calculaMedia(){
-        return sumaDeLasEvaluaciones / totalDeLasEvalaciones;
+    //SETTERS
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
