e4576e5be0e6462839d6679b4d8d55c272f8aa72
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-10-23 11:06:36 -0600

    Crea clase Episodio

---
+++ src/aluracursos/screenmatch/modelos/Episodio.java
@@ -0,0 +1,47 @@
+package aluracursos.screenmatch.modelos;
+
+import aluracursos.screenmatch.calculos.Clasificacion;
+
+public class Episodio implements Clasificacion {
+/* ATRIBUTOS */
+
+    private int numero;
+    private String nombre;
+    private Serie serie;
+    private int totalVisualizaciones;
+
+/* METODOS */
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    @Override
+    public int getClasificacion() {
+        if (totalVisualizaciones > 100){
+            return 4;
+        } else {
+            return 2;
+        }
+    }
+}
