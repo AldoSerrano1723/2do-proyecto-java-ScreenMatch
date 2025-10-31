c6ec849f4766d382fbb28aa399218dd96c656f43
AldoSerrano1723 <aldoantonio1723@gmail.com>
2025-10-31 11:50:44 -0600

    Actualiza PrincipalConListas

--- src/aluracursos/screenmatch/principal/PrincipalConListas.java
+++ src/aluracursos/screenmatch/principal/PrincipalConListas.java
@@ -9,16 +9,23 @@ import java.util.ArrayList;
 public class PrincipalConListas {
     public static void main(String[] args){
         Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.evalua(9);
         Serie casaDragon = new Serie ("La cada del dragon", 2022);
-        var otraPelicual = new Pelicula("Alien", 1979);
+        casaDragon.evalua(8);
+        var otraPelicula = new Pelicula("Alien", 1979);
+        otraPelicula.evalua(10);
 
         ArrayList<Titulo> lista = new ArrayList<>();
-        lista.add(otraPelicual);
         lista.add(miPelicula);
         lista.add(casaDragon);
+        lista.add(otraPelicula);
 
         for (Titulo item : lista){
+            System.out.println("----------------------------------------------------------------------------");
             System.out.println(item);
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion() > 2){
+                System.out.println("La pelicual tiene " + pelicula.getClasificacion() + " Estrellas");
+            }
         }
 
 
